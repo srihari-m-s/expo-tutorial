33633dcc1110198ce723c9944b74b69d3ac624b9
srihari-m-s <srihari.magadi@gmail.com>
2025-12-26 16:14:41 +0530

    Add about screen

---
+++ app/about.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View className="flex-1 items-center justify-center bg-stone-800 gap-6">
+      <Text className="text-xl font-bold text-white">About screen</Text>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-stone-800">
+    <View className="flex-1 items-center justify-center bg-stone-800 gap-6">
       <Text className="text-xl font-bold text-white">Home screen</Text>
+
+      <Link href={"/about"} className="text-xl underline text-white">
+        About
+      </Link>
     </View>
   );
 }
