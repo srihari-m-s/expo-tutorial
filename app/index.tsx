6e3b432995c7c5f7ec6d18aab9447f5a13989905
srihari-m-s <srihari.magadi@gmail.com>
2025-12-26 16:12:32 +0530

    Add native wind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-stone-800">
+      <Text className="text-xl font-bold text-white">Home screen</Text>
     </View>
   );
 }
